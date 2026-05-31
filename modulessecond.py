6672d8aea4a7b1cdc48174fed2ea35789551d13c
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-05-31 13:53:21 +0545

    Different types of modules in python

---
+++ modulessecond.py
@@ -0,0 +1,39 @@
+# import math
+# #print(dir(math))
+# print(math.floor(3.16))
+# print(math.sin(3.6))
+
+#random module
+# import random
+# # for i in range(10):
+# #   print(random.random())
+
+# #euta random number dinxa
+# print(random.randint(1,6))
+
+
+
+#random floating point define garna uniform() function use garxau
+# for i in range(10):
+#   print(random.uniform.uniform(1000,2000))
+
+# print(random.randrange(10))
+# print(random.randrange(1,10))
+# print(random.randrange(1,10,2))
+
+# from random import *
+# list=["Rabin","Sabin","Pragyan","Nikhil"]
+# print(choice(list))
+
+
+#opt generation (6 number)
+# from random import *
+# print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
+
+# from random import *
+# otp=''
+# for i in range(6):
+#   otp=otp+str(randint(0,9))
+# print(otp)
+
+#Generate fake employee data for database purpose
