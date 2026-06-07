7e1ff47fd8332f851ec2d315356e1dfb0ccccee3
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-07 08:25:55 +0545

    Concept of methodoverriding in python

---
+++ methodoverriding.py
@@ -0,0 +1,21 @@
+# class P:
+#   def property(self):
+#     print("Gold + Land + Cash ")
+#   def wife(self):
+#     print("kajol")
+# class C(P):
+#   def wife(self):
+#    # super().wife()#kajol nai chahiyo vani
+#     print("Katrina")
+# c=C()
+# c.property()
+# c.wife()
+
+class P:
+  def __init__(self):
+    print("Parent class constructor")
+class C(P):
+  def __init__(self):
+    super().__init__()# parent ko call garna
+    print("Child class constructor")
+c=C()
