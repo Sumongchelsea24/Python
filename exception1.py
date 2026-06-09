2f7d0179e23bcfcbb284d1cf1ab2939a2bb6b252
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-09 08:58:32 +0545

    Concept of else block with try except finally and User defined Exceptions in python

--- exception1.py
+++ exception1.py
@@ -1,13 +1,41 @@
-try:
-  print("Outer try block")
-  try:
-    print("Inner try block")
-    print(10/0)
-  except ZeroDivisionError:
-    print("Inner except block")
-  finally:
-    print("Inner finally block")
-except:
-  print("Outer except block.")
-finally:
-  print("Outer finally block")
+# try:
+#   print("Outer try block")
+#   try:
+#     print("Inner try block")
+#     print(10/0)
+#   except ZeroDivisionError:
+#     print("Inner except block")
+#   finally:
+#     print("Inner finally block")
+# except:
+#   print("Outer except block.")
+# finally:
+#   print("Outer finally block")
+
+# try:
+#   print("try")
+#   print(10/0)
+# except:
+#   print("Except")
+# else:
+#   print("Else")
+# finally:
+#   print("Finally")
+
+#Example of userdefined exception
+
+class TooYoungException(Exception):
+  def __init__(self, args):
+    self.msg=args
+
+class TooOldException(Exception):
+  def __init__(self, args):
+    self.msg=args
+    
+age=int(input("Enter Your Age : "))
+if age>60:
+  raise TooOldException("Oh! Wait some more years and you will get a better one.")
+elif age<18:
+  raise TooYoungException("Oh! boy focus on your study !")
+else:
+  print("You will get your match! Wait sometime !")
