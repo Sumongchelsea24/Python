2460dc41db44e7054c9f860d95934c7d1f2e2754
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-14 08:43:49 +0545

    Concept of python documention

---
+++ mydoc.py
@@ -0,0 +1,12 @@
+#import math
+#print(dir(math))
+#help(math)
+
+import mytest1
+#help(mytest1)
+print("--- Module Documentation ---")
+print(mytest1.__doc__)
+
+print("\n--- Meta Data ---")
+print("Author:", mytest1.__author__)
+print("Copyright:", mytest1.__copyright__)
---
+++ mytest1.py
@@ -0,0 +1,3 @@
+"""This module contains test scripts of my software ... I feel excited to be part of this coding class"""
+__author__="Chelsea and Team"
+__copyright__="2026 Chelsea"
