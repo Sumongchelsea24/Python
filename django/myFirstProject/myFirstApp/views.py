3f090dcde56eaf1c54b871fddc0d1bf95a6428f4
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-15 07:54:35 +0545

    Giving heading tag and style in views.py

--- django/myFirstProject/myFirstApp/views.py
+++ django/myFirstProject/myFirstApp/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def display(request):
-  s="Hello this is my first request."
+  s = '<h1 style="color: blue; text-align: center;">Hello this is my first request.</h1>'
   return HttpResponse(s)
+ 
