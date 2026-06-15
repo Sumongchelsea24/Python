2b176b8f842a4d5bee9015e8082281d9ba2f5278
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-15 09:38:29 +0545

    Creating wish app l

--- django/myFirstProject/myFirstApp/views.py
+++ django/myFirstProject/myFirstApp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+import datetime
 
 
 # Create your views here.
 def display(request):
-  my_dict={
-    'name':"Rahul",
-     'age':20
+  date=datetime.datetime.now()
+  msg="Hello Guest !!"
+  h=int(date.strftime("%H"))
+  if h<12:
+    msg+="Good Morning."
+  elif h<16:
+    msg+="Good Afternoon."
+  elif h<20:
+    msg+="Good Evening."
+  else:
+    msg+="Hello Guest, Good night."
+  context={'insert_date':date,'insert_msg':msg
+    
   }
-  return render(request,'myFirstApp/html/index.html',context=my_dict)
+  return render(request,'myFirstApp/html/index.html',context=context)
  
