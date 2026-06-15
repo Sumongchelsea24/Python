517d388181541cb2dcc318e9048b160f33c50cdc
Sumongchelsea24 <Sujanchelsea24@gmail.com>
2026-06-15 09:05:32 +0545

    Throwing context from view and catching it in html

--- django/myFirstProject/myFirstApp/views.py
+++ django/myFirstProject/myFirstApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def display(request):
-  s = '<h1 style="color: blue; text-align: center;">Hello this is my first request.</h1>'
-  return HttpResponse(s)
+  my_dict={
+    'name':"Rahul",
+     'age':20
+  }
+  return render(request,'myFirstApp/html/index.html',context=my_dict)
  
